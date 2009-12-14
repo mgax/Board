6e212e9811b222e755c4ef49609d74ba50c453e8
Alex Morega <alex@grep.ro>
2009-12-14 22:32:43 +0200

    browser tests: move support functions to main page

--- board/tests/browser_test_media/test_load_model.js
+++ board/tests/browser_test_media/test_load_model.js
@@ -14,30 +14,4 @@ test("Load model", function() {
     }));
 });
 
-function try_catch(callback) {
-    return function() {
-        try {
-            callback.apply(this, arguments);
-        }
-        catch(e) {
-            console.error(e);
-            assertTrue(false, "Exception caught:" + e);
-            start();
-        }
-    }
-}
-
-function load_fixture(data, callback) {
-    $.ajax({
-        url: '/load_fixture',
-        type: 'POST',
-        data: {'fixture': data},
-        success: callback,
-        error: function(evt) {
-            assertTrue(false, "AJAX call failed");
-            start();
-        }
-    });
-}
-
 });
