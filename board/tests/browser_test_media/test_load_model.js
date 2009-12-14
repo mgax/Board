4d12a6b494710d7d0fa2186dea90f4f3502b177d
Alex Morega <alex@grep.ro>
2009-12-14 22:59:17 +0200

    expanding and refactoring note load tests

--- board/tests/browser_test_media/test_load_model.js
+++ board/tests/browser_test_media/test_load_model.js
@@ -2,16 +2,39 @@ $(function() {
 
 module("Note model");
 
-test("Load model", function() {
+test("Load note", function() {
     var fixture = '{properties: {a: "b"}, children: []}';
     stop();
     load_fixture(fixture, try_catch(function() {
-        board.load_note_at_url('/root', try_catch(function(evt) {
-            assertTrue(evt.success, "Note load event success");
-            assertSame(evt.note.properties, {a: 'b'});
-            start();
-        }));
+        load_and_check_note('/root', {a: 'b'}, start); }));
+});
+
+test("Load sub note", function() {
+    var fixture = '{properties: {}, children:[' +
+            '{properties: {"-name-": "a"}, children: [' +
+                '{properties: {"-name-": "c"}, children: []}' +
+            ']}, ' +
+            '{properties: {"-name-": "b"}, children: []}' +
+        ']}';
+    stop();
+    load_fixture(fixture, try_catch(function() {
+        var queue = [];
+        queue.push(function(callback) {
+            load_and_check_note('/root/a', {"-name-": "a"}, callback); });
+        queue.push(function(callback) {
+            load_and_check_note('/root/b', {"-name-": "b"}, callback); });
+        queue.push(function(callback) {
+            load_and_check_note('/root/a/c', {"-name-": "c"}, callback); });
+        testQueue(queue, start);
     }));
 });
+
+function load_and_check_note(url, properties, callback) {
+    board.load_note_at_url(url, try_catch(function(evt) {
+        assertTrue(evt.success, "Note load event success");
+        assertSame(evt.note.properties, properties);
+        callback();
+    }));
+}
 
 });
